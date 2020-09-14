846b51dbada1568e8fa7cc3a5317f75fcf622cb4
MasterElp <vener.galiullin@gmail.com>
2020-09-14 16:53:46 +0500

    add User, UserInterface

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+#pip install keyboard
 import esper
 import time
 import random
+import keyboard
+
+class User:
+    def __init__(self):
+        pass
 
 class Relations:
     def __init__(self):
@@ -15,72 +21,55 @@ class Position:
     def __init__(self, location):
         self.location = location
 
+class UserInterfaceProcessor(esper.Processor):
+    def __init__(self, user):
+        super().__init__()
+        self.user = user
+
+    def process(self):
+        #for user, user_component in self.world.get_component(User):
+        
+        if keyboard.is_pressed("p"):
+            print("You pressed p")
+        if keyboard.is_pressed("s"):
+            location = self.world.component_for_entity(self.user, Position).location
+            print("location: ", location)
+            print(location_contain(self.world, location))
+
 
 class PositionProcessor(esper.Processor):
     def __init__(self):
         super().__init__()
 
     def process(self):
-        self.show_all_locations()
+        #self.show_all_locations()
         pass
-
-    def location_contain(self, location):
-        objects_contain = []
-        for inside_object, position in self.world.get_component(Position):
-            if (position.location == location):
-                objects_contain.append(inside_object)
-
-        return objects_contain
 
     def show_all_locations(self):
         for entity, location in self.world.get_components(Location):
             print("location: ", entity)
-            print(self.location_contain(entity))
-
-"""class Vision:
-    def __init__(self):
-        self.objects_count = 0
-
-class MovementProcessor(esper.Processor):
-    def __init__(self):
-        super().__init__()
-
-    def process(self):
-        for ent, (vel, pos) in self.world.get_components(Velocity, Position):
-            #pos.x += vel.x
-            #pos.y += vel.y
-            #print("Current Position: {}".format((int(pos.x), int(pos.y))))
-            pass
-
-class VisionProcessor(esper.Processor):
-    def __init__(self):
-        super().__init__()
-
-    def process(self):
-        print("X")
-        for ent, (vision, my_pos) in self.world.get_components(Vision, Position):
-            vision.objects_count = 0
-            print("My position: {}".format((int(my_pos.x), int(my_pos.y))))
-            for other, pos in self.world.get_component(Position):
-                #print("other: {}".format((int(pos.x), int(pos.y))))
-                if ((my_pos.x != pos.x) and (my_pos.y != pos.y)):
-                    vision.objects_count += 1
-                    print("Other object: {}".format((other, int(pos.x), int(pos.y))))"""
+            print(location_contain(self.world, entity))
 
 
+
+def location_contain(world, location):
+    
+    objects_contain = []
+    for inside_object, position in world.get_component(Position):
+        if (position.location == location):
+            objects_contain.append(inside_object)
+
+    return objects_contain
 
 def main():
     # Create a World instance to hold everything:
     world = esper.World()
 
-    # Instantiate a Processor (or more), and add them to the world:
-    movement_processor = PositionProcessor()
-    world.add_processor(movement_processor)
-
     area = world.create_entity(Location())
     glade = world.create_entity(Location(), Position(area))
     home = world.create_entity(Location(), Position(area))
 
+    user = world.create_entity(User(), Relations(), Position(glade))
 
     goblin = []
     # Create entities, and assign Component instances to them:
@@ -90,6 +79,9 @@ def main():
         #world.add_component(player[i], Position(x=5, y=5))
         #world.add_component(player[i], Vision())
 
+    # Instantiate a Processor (or more), and add them to the world:
+    world.add_processor(PositionProcessor())
+    world.add_processor(UserInterfaceProcessor(user))
 
 
     # A dummy main loop:
@@ -101,6 +93,8 @@ def main():
 
     except KeyboardInterrupt:
         return
+
+
 
 if __name__ == "__main__":
     main()
